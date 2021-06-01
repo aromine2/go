2285424109e8a60ae57c5aab27571305f2bf6706
Alex Romine <alexromine3@gmail.com>
2021-05-31 19:13:11 -0600

    working, but no fully dry

--- process-financials/main.go
+++ process-financials/main.go
@@ -6,24 +6,25 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	// Open the file
 	r := createFileReader()
 
-	// Iterate through the records
-	count := sortFileContents(r)
-	fmt.Println(count)
+	sortFileContents(r)
 }
 
-func sortFileContents(r *csv.Reader) int {
-	count := 0
+func sortFileContents(r *csv.Reader) {
+	// Categories to sort transactions by
 	var (
-		amazonTotal float64 = 0
+		amazonTotals = make(map[string]float64)
 	)
+
 	for {
 		// Read each transaction from csv
 		transaction, err := r.Read()
@@ -34,25 +35,53 @@ func sortFileContents(r *csv.Reader) int {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%s\n", transaction)
-		amazonTotal = filterTransactions("AMZN", transaction, amazonTotal)
-		amazonTotal = filterTransactions("Amazon", transaction, amazonTotal)
+		amazonTotals = filterTransactions("AMZN", amazonTotals, transaction)
+		amazonTotals = filterTransactions("Amazon", amazonTotals, transaction)
 	}
-	fmt.Printf("amazon: %.2f", amazonTotal)
-	return count
+
+	printCategoryResults(amazonTotals)
 }
 
-func filterTransactions(filterString string, transaction []string, categorySpend float64) float64 {
+func printCategoryResults(amazonTotals map[string]float64) {
+	keys := make([]string, 0, len(amazonTotals))
+	for k := range amazonTotals {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	fullTotal := 0.00
+	for _, k := range keys {
+		fmt.Printf("%s: %.2f\n", k, amazonTotals[k])
+		fullTotal = fullTotal + amazonTotals[k]
+	}
+	fmt.Printf("full total: %.2f\n", fullTotal)
+}
+
+func filterTransactions(filterString string, totalCategorySpend map[string]float64, transaction []string) map[string]float64 {
 
 	if strings.Contains(transaction[2], filterString) {
 		transactionAmount, err := strconv.ParseFloat(transaction[5], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		categorySpend = categorySpend + transactionAmount
+
+		const transactionLayout = "01/02/2006"
+		const sortingLayout= "2006 01"
+	    transactionDate, err := time.Parse(transactionLayout, transaction[0])
+	    if err != nil {
+	    	log.Fatal(err)
+		}
+
+		monthSpend := transactionDate.Format(sortingLayout)
+		totalCategorySpend[monthSpend] = totalCategorySpend[monthSpend] + transactionAmount
+	    //if _, ok := totalCategorySpend[monthSpend]; ok {
+		//	totalCategorySpend[monthSpend] = totalCategorySpend[monthSpend] + transactionAmount
+		//} else {
+		//	totalCategorySpend[monthSpend] = transactionAmount
+		//}
 	}
 
-	return categorySpend
+	return totalCategorySpend
 }
 
 func createFileReader() *csv.Reader {
@@ -64,6 +93,5 @@ func createFileReader() *csv.Reader {
 
 	// Parse the file
 	r := csv.NewReader(csvfile)
-	//r := csv.NewReader(bufio.NewReader(csvfile))
 	return r
 }
