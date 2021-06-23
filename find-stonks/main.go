2be8651f4d1c87b0f89878c34a414130cba4f152
Alex Romine <alexromine3@gmail.com>
2021-06-22 22:48:11 -0600

    stonks only go up. Also, working on reading the json response data

--- find-stonks/main.go
+++ find-stonks/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
+type balanceSheetResponse struct {
+	Symbol    string
+	AnnualReports   []map[string]string
+	QuarterlyReports   []map[string]string
+}
+
 func main() {
 	stockSymbol := processArgs()
-
 	apiKey := os.Getenv("ALPHA_ADVANTAGE_PW")
 	stockFunction := "BALANCE_SHEET"
+
+	balanceSheet := getInfoFromApi(stockFunction, stockSymbol, apiKey)
+	balanceSheetObject := processBalanceSheet(balanceSheet)
+
+	// Need to find a way to filter on the latest
+	fmt.Println(balanceSheetObject.AnnualReports[3]["longTermDebt"])
+}
+
+func processBalanceSheet(balanceSheet []byte) balanceSheetResponse {
+	var stockBalanceSheet balanceSheetResponse
+
+	err := json.Unmarshal(balanceSheet, &stockBalanceSheet)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return stockBalanceSheet
+}
+
+func getInfoFromApi(stockFunction string, stockSymbol string, apiKey string) []byte {
 	url := fmt.Sprintf("https://www.alphavantage.co/query?function=%s&symbol=%s&apikey=%s", stockFunction, stockSymbol, apiKey)
 
 	req, _ := http.NewRequest("GET", url, nil)
@@ -20,7 +47,5 @@ func main() {
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	fmt.Println(res)
-	fmt.Println(string(body))
-
+	return body
 }
